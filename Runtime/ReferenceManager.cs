a32868bc7cf8a659d4b5274f0b91629b1c048d3d
Ace <tk009999@gmail.com>
2025-01-10 11:28:20 +0800

    1.0.2.1 update reference manager

--- Runtime/ReferenceManager.cs
+++ Runtime/ReferenceManager.cs
@@ -1,96 +1,99 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IReference
+namespace GSGUnityUtilities.Runtime
 {
-    public void Init();
-    public void Release();
-}
-
-public interface IReferenceManager : IReference
-{
-    Dictionary<string, IReference> ReferenceMap { get; }
-    void AddReference<T>(T reference) where T : IReference;
-    void RemoveReference<T>(T reference) where T : IReference;
-    T GetReference<T>() where T : class, IReference;
-    bool ContainsReference<T>() where T : class, IReference;
-}
-
-public class ReferenceManager : Singleton<ReferenceManager>, IReferenceManager
-{
-    public Dictionary<string, IReference> ReferenceMap { get; private set; } = new Dictionary<string, IReference>();
-
-    public void Init()
+    public interface IReference
     {
-        ReferenceMap = new Dictionary<string, IReference>()
+        public void Init();
+        public void Release();
+    }
+
+    public interface IReferenceManager : IReference
+    {
+        Dictionary<string, IReference> ReferenceMap { get; }
+        void AddReference<T>(T reference) where T : IReference;
+        void RemoveReference<T>(T reference) where T : IReference;
+        T GetReference<T>() where T : class, IReference;
+        bool ContainsReference<T>() where T : class, IReference;
+    }
+
+    public class ReferenceManager : Singleton<ReferenceManager>, IReferenceManager
+    {
+        public Dictionary<string, IReference> ReferenceMap { get; private set; } = new Dictionary<string, IReference>();
+
+        public virtual void Init()
+        {
+            ReferenceMap = new Dictionary<string, IReference>()
         {
             { typeof(IReferenceManager).Name, this },
         };
 
-        Debug.Log("ReferenceManager Init");
-    }
-
-    public void Release()
-    {
-        ReferenceMap.Clear();
-        Debug.Log("ReferenceManager Release");
-    }
-
-    // 添加引用
-    public void AddReference<T>(T reference) where T : IReference
-    {
-        string key = typeof(T).Name;
-        Debug.Log($"ReferenceManager: AddReference {key}");
-
-        if (!ReferenceMap.ContainsKey(key))
-        {
-            ReferenceMap.Add(key, reference);
+            Debug.Log("ReferenceManager Init");
         }
-        else
+
+        public virtual void Release()
         {
-            Debug.LogWarning($"ReferenceManager: Key {key} already exists.");
+            ReferenceMap.Clear();
+            Debug.Log("ReferenceManager Release");
         }
-    }
 
-    // 获取引用
-    public T GetReference<T>() where T : class, IReference
-    {
-        string key = typeof(T).Name;
-        Debug.Log($"ReferenceManager: GetReference {key}");
-
-        if (ReferenceMap.ContainsKey(key))
+        // 添加引用
+        public virtual void AddReference<T>(T reference) where T : IReference
         {
-            return ReferenceMap[key] as T;
+            string key = typeof(T).Name;
+            Debug.Log($"ReferenceManager: AddReference {key}");
+
+            if (!ReferenceMap.ContainsKey(key))
+            {
+                ReferenceMap.Add(key, reference);
+            }
+            else
+            {
+                Debug.LogWarning($"ReferenceManager: Key {key} already exists.");
+            }
         }
-        else
+
+        // 获取引用
+        public virtual T GetReference<T>() where T : class, IReference
         {
-            Debug.LogWarning($"ReferenceManager: Key {key} not found.");
-            return null;
+            string key = typeof(T).Name;
+            Debug.Log($"ReferenceManager: GetReference {key}");
+
+            if (ReferenceMap.ContainsKey(key))
+            {
+                return ReferenceMap[key] as T;
+            }
+            else
+            {
+                Debug.LogWarning($"ReferenceManager: Key {key} not found.");
+                return null;
+            }
         }
-    }
 
-    // 检查是否包含引用
-    public bool ContainsReference<T>() where T : class, IReference
-    {
-        string key = typeof(T).Name;
-        Debug.Log($"ReferenceManager: ContainsReference {key}");
-
-        return ReferenceMap.ContainsKey(key);
-    }
-
-    // 移除引用
-    public void RemoveReference<T>(T reference) where T : IReference
-    {
-        string key = typeof(T).Name;
-        Debug.Log($"ReferenceManager: RemoveReference {key}");
-
-        if (ReferenceMap.ContainsKey(key))
+        // 检查是否包含引用
+        public virtual bool ContainsReference<T>() where T : class, IReference
         {
-            ReferenceMap.Remove(key);
+            string key = typeof(T).Name;
+            Debug.Log($"ReferenceManager: ContainsReference {key}");
+
+            return ReferenceMap.ContainsKey(key);
         }
-        else
+
+        // 移除引用
+        public virtual void RemoveReference<T>(T reference) where T : IReference
         {
-            Debug.LogWarning($"ReferenceManager: Key {key} not found.");
+            string key = typeof(T).Name;
+            Debug.Log($"ReferenceManager: RemoveReference {key}");
+
+            if (ReferenceMap.ContainsKey(key))
+            {
+                ReferenceMap.Remove(key);
+            }
+            else
+            {
+                Debug.LogWarning($"ReferenceManager: Key {key} not found.");
+            }
         }
     }
 }
