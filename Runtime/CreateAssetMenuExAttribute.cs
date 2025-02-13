b24c7a84df5629563968e785d2ef9c42002477af
Ace <tk009999@gmail.com>
2025-02-13 15:18:47 +0800

    remove namespace

--- Runtime/CreateAssetMenuExAttribute.cs
+++ Runtime/CreateAssetMenuExAttribute.cs
@@ -3,18 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace GSGUnityUtilities.Runtime
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public class CreateAssetMenuExAttribute : Attribute
 {
-    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
-    public class CreateAssetMenuExAttribute : Attribute
-    {
-        public string MenuName { get; }
-        public string FileName { get; }
+    public string MenuName { get; }
+    public string FileName { get; }
 
-        public CreateAssetMenuExAttribute(string menuName, string fileName = null)
-        {
-            MenuName = menuName;
-            FileName = fileName ?? "New " + menuName;
-        }
+    public CreateAssetMenuExAttribute(string menuName, string fileName = null)
+    {
+        MenuName = menuName;
+        FileName = fileName ?? "New " + menuName;
     }
 }
